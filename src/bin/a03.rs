ae1e904f1bce6da9859e12ada4168127227246e4
ikatama <ikatama@example.com>
2023-06-03 02:52:19 +0900

    solved a03

--- src/bin/a03.rs
+++ src/bin/a03.rs
@@ -1,3 +1,20 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: i32,
+        k: i32,
+        p: [i32;n],
+        q: [i32;n]
+    }
+
+    for a in p {
+        let b = k - a;
+        if q.contains(&b) {
+            println!("Yes");
+            return;
+        }
+    }
+
+    println!("No");
 }
