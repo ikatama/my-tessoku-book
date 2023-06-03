809f90bd42faf3b32a1f10928c36a2e7d83b4e6e
ikatama <ikatama@example.com>
2023-06-03 14:58:21 +0900

    solved a04

--- src/bin/a04.rs
+++ src/bin/a04.rs
@@ -1,3 +1,27 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: i32,
+    }
+    let converted = convert_radix(n, 2);
+    let result: String = converted
+        .iter()
+        .map(|n| n.to_string())
+        .collect::<Vec<String>>()
+        .concat();
+
+    println!("{:0>10}", result);
+}
+
+fn convert_radix(mut n: i32, b: i32) -> Vec<i32> {
+    let mut remainder: Vec<i32> = Vec::new();
+
+    while n != 0 {
+        remainder.push(n % b);
+        n = n / b;
+    }
+
+    remainder.reverse();
+    return remainder;
 }
