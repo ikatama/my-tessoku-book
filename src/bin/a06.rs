56566af9671fdfde1a071a73aa924d285abc5a05
ikatama <ikatama@example.com>
2023-06-04 02:26:02 +0900

    solved a06

--- src/bin/a06.rs
+++ src/bin/a06.rs
@@ -1,3 +1,18 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: i32,
+        q: i32,
+        a: [i32;n],
+    }
+
+    for _ in 1..=q {
+        input! {
+            l: usize,
+            r: usize,
+        }
+        let sum = a[l - 1..r].iter().fold(0, |sum, i| sum + i);
+        println!("{}", sum);
+    }
 }
