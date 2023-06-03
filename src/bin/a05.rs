15a0bdb39fbf2c22b2687f36acda35baaea7034f
ikatama <ikatama@example.com>
2023-06-03 15:42:24 +0900

    solved a05

--- src/bin/a05.rs
+++ src/bin/a05.rs
@@ -1,3 +1,18 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: i32,
+        k: i32,
+    }
+
+    let mut count = 0;
+    for a in 1..=n {
+        for b in 1..=n {
+            if a + b < k && a + b >= k - n {
+                count += 1;
+            }
+        }
+    }
+    println!("{}", count);
 }
