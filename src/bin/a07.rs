bc19056195b668a7ba5b417b67eff4b3e4375202
ikatama <ikatama@example.com>
2023-06-05 01:01:50 +0900

    solved a07

--- src/bin/a07.rs
+++ src/bin/a07.rs
@@ -1,3 +1,27 @@
+use std::vec;
+
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        d: usize,
+        n: usize,
+    }
+
+    let mut v: Vec<i32> = vec![0; d];
+
+    for _ in 0..n {
+        input! {
+            l: usize,
+            r: usize,
+        }
+
+        for i in l..=r {
+            v[i - 1] += 1;
+        }
+    }
+
+    for e in v {
+        println!("{}", e);
+    }
 }
