b667b20ad06305f27a4497a38a32200f0491394c
ikatama <ikatama@example.com>
2023-06-06 23:34:33 +0900

    solved a08

--- src/bin/a08.rs
+++ src/bin/a08.rs
@@ -1,3 +1,25 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        h: usize,
+        w: usize,
+        x: [[usize; w]; h],
+        q: usize,
+    }
+
+    for _ in 0..q {
+        input! {
+            a: usize,
+            b: usize,
+            c: usize,
+            d: usize,
+        }
+
+        let mut sum: usize = 0;
+        for y in x[a - 1..c].iter() {
+            sum += y[b - 1..d].iter().fold(0, |sum, s| sum + s);
+        }
+        println!("{}", sum);
+    }
 }
