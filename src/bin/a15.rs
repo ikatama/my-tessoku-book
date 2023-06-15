0246b8f5848108fac2344197183e4888cfcb782b
ikatama <ikatama@example.com>
2023-06-16 00:59:26 +0900

    solved a15

--- src/bin/a15.rs
+++ src/bin/a15.rs
@@ -1,3 +1,38 @@
+use itertools::Itertools;
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [usize; n],
+    }
+
+    let sorted_a: Vec<usize> = {
+        let mut b: Vec<usize> = a.clone();
+        b.sort();
+        b.into_iter().unique().collect_vec()
+    };
+
+    let mut compressed: Vec<usize> = vec![0; n];
+
+    for i in 0..n {
+        let mut left = 0;
+        let mut right = sorted_a.len();
+        while left < right {
+            let mid = (left + right) / 2;
+            if a[i] == sorted_a[mid] {
+                compressed[i] = mid;
+                break;
+            } else if a[i] < sorted_a[mid] {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+    }
+
+    println!(
+        "{}",
+        compressed.iter().map(|x| (x + 1).to_string()).join(" ")
+    );
 }
