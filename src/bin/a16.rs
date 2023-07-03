8254a878e7434eba4ed4b07c7e5e46c69c6286e5
ikatama <ikatama@example.com>
2023-07-04 01:28:58 +0900

    solved a16

--- src/bin/a16.rs
+++ src/bin/a16.rs
@@ -1,3 +1,23 @@
+use std::cmp;
+
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [usize; n-1],
+        b: [usize; n-2],
+    }
+
+    // 動的計画法
+    let mut dp = vec![0; n];
+
+    dp[0] = 0;
+    dp[1] = a[0];
+
+    for i in 2..n {
+        dp[i] = cmp::min(dp[i - 1] + a[i - 1], dp[i - 2] + b[i - 2]);
+    }
+
+    println!("{}", dp.last().unwrap());
 }
