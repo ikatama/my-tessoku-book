cf659267d7b828ac505bb14d46dbb8e3e4065487
ikatama <ikatama@example.com>
2023-07-07 01:49:10 +0900

    solved a20

--- src/bin/a20.rs
+++ src/bin/a20.rs
@@ -1,3 +1,29 @@
+use std::cmp;
+
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        s: String,
+        t: String,
+    }
+
+    let s: Vec<char> = s.chars().collect();
+    let t: Vec<char> = t.chars().collect();
+
+    let mut dp_table = vec![vec![0; t.len() + 1]; s.len() + 1];
+
+    for i in 1..=s.len() {
+        for j in 1..=t.len() {
+            if s[i - 1] == t[j - 1] {
+                dp_table[i][j] = cmp::max(
+                    cmp::max(dp_table[i - 1][j], dp_table[i][j - 1]),
+                    dp_table[i - 1][j - 1] + 1,
+                );
+            } else {
+                dp_table[i][j] = cmp::max(dp_table[i - 1][j], dp_table[i][j - 1]);
+            }
+        }
+    }
+    println!("{}", dp_table[s.len()][t.len()]);
 }
