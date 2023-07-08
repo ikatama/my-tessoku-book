4bf4674ba80ae1ae96eead5bff40e4b9d3ff0786
ikatama <ikatama@example.com>
2023-07-08 15:37:47 +0900

    solved a22

--- src/bin/a22.rs
+++ src/bin/a22.rs
@@ -1,3 +1,20 @@
+use std::cmp;
+
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [usize; n-1],
+        b: [usize; n-1],
+    }
+
+    let mut dp = vec![0; n + 1];
+
+    for i in 1..n {
+        dp[a[i - 1]] = cmp::max(dp[a[i - 1]], dp[i] + 100);
+        dp[b[i - 1]] = cmp::max(dp[b[i - 1]], dp[i] + 150);
+    }
+
+    println!("{}", dp[n]);
 }
