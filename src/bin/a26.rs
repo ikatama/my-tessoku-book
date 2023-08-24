09b4ed1db0707e8746f51327bde95c56015224b8
ikatama <ikatama@example.com>
2023-08-24 14:39:36 +0000

    solved a26

--- src/bin/a26.rs
+++ src/bin/a26.rs
@@ -1,3 +1,29 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        q: usize,
+        x: [u32; q],
+    }
+
+    for i in x {
+        if is_prime(i) {
+            println!("Yes");
+        } else {
+            println!("No");
+        }
+    }
+}
+
+fn is_prime(n: u32) -> bool {
+    let mut i = 2;
+
+    while i * i <= n {
+        if n % i == 0 {
+            return false;
+        }
+        i += 1;
+    }
+
+    return true;
 }
