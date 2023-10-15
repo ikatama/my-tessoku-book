7817d00d164397265f2d5f1a8a1e5407011c3a4d
ikatama <ikatama@example.com>
2023-10-15 15:25:33 +0000

    solved a27

--- src/bin/a27.rs
+++ src/bin/a27.rs
@@ -1,3 +1,19 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        a: u32,
+        b: u32,
+    }
+
+    // ユークリッドの互除法
+    println!("{}", gcd(a, b))
+}
+
+fn gcd(a: u32, b: u32) -> u32 {
+    if a == 0 {
+        return b;
+    } else {
+        gcd(b % a, a)
+    }
 }
