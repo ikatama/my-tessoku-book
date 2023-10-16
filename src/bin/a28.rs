a13b5357729ba30efb5ba1f7b27fc33d4c5f154e
ikatama <ikatama@example.com>
2023-10-16 17:06:45 +0000

    solved a28

--- src/bin/a28.rs
+++ src/bin/a28.rs
@@ -1,3 +1,21 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: i32,
+        t_a: [(char, i32); n],
+    }
+
+    let mut result = 0;
+    for t_a_iter in t_a.into_iter() {
+        match t_a_iter.0 {
+            '+' => result += t_a_iter.1,
+            '-' => result -= t_a_iter.1,
+            '*' => result *= t_a_iter.1,
+            _ => panic!(),
+        }
+        result = if result < 0 { result + 10000 } else { result };
+        result %= 10000;
+        println!("{}", result);
+    }
 }
