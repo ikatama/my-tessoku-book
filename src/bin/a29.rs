4b611342db96f3dc7607aac46372e5394391fcb4
ikatama <ikatama@example.com>
2023-10-18 15:27:41 +0000

    solved a29

--- src/bin/a29.rs
+++ src/bin/a29.rs
@@ -1,3 +1,25 @@
+use num_traits::PrimInt;
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        a: u64,
+        b: u64,
+    }
+
+    let p = 10.pow(9) + 7;
+    let result = modpow(a, b, p);
+    println!("{}", result);
+}
+
+fn modpow(mut a: u64, mut b: u64, p: u64) -> u64 {
+    let mut result = 1;
+    while b > 0 {
+        if b & 1 > 0 {
+            result = result * a % p
+        }
+        a = a * a % p;
+        b >>= 1;
+    }
+    result
 }
